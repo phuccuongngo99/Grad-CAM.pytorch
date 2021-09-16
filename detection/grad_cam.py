859546969cb0b58dbb76bc74444fca5856e49083
Cuong Ngo <cuongngo@u.nus.edu>
2021-09-17 00:00:05 +0800

    Add method to efficiently compute heatmap for all detection

--- detection/grad_cam.py
+++ detection/grad_cam.py
@@ -83,6 +83,59 @@ class GradCAM(object):
         class_id = output[0]['instances'].pred_classes[index].detach().numpy()
         return cam, box, class_id
 
+    def get_mask_all_detection(self, inputs):
+        """
+
+        :param inputs: {"image": [C,H,W], "height": height, "width": width}
+        :param index: 第几个边框
+        :return:
+        """
+        self.net.zero_grad()
+        output = self.net.inference([inputs])
+        print(output)
+
+        # Number of detection
+        num_det = len(output[0]['instances'])
+        list_dict = []
+
+        for index in range(num_det):
+            # Reset the gradient
+            self.net.zero_grad()
+
+            score = output[0]['instances'].scores[index]
+            proposal_idx = output[0]['instances'].indices[index]  # box来自第几个proposal
+            score.backward(retain_graph=True)
+
+            gradient = self.gradient[proposal_idx].cpu().data.numpy()  # [C,H,W]
+            weight = np.mean(gradient, axis=(1, 2))  # [C]
+
+            feature = self.feature[proposal_idx].cpu().data.numpy()  # [C,H,W]
+
+            cam = feature * weight[:, np.newaxis, np.newaxis]  # [C,H,W]
+            cam = np.sum(cam, axis=0)  # [H,W]
+            cam = np.maximum(cam, 0)  # ReLU
+
+            # 数值归一化
+            cam -= np.min(cam)
+            cam /= np.max(cam)
+            # resize to 224*224
+            box = output[0]['instances'].pred_boxes.tensor[index].detach().numpy().astype(np.int32)
+            x1, y1, x2, y2 = box
+            cam = cv2.resize(cam, (x2 - x1, y2 - y1))
+
+            class_id = output[0]['instances'].pred_classes[index].detach().numpy()
+            
+            # Putting the result in a dict
+            obj_dict = {}
+            obj_dict['cam'] = cam
+            obj_dict['box'] = box
+            obj_dict['class_id'] = class_id
+            obj_dict['conf'] = score.item()
+            
+            list_dict.append(obj_dict)
+ 
+        return list_dict
+
 
 class GradCamPlusPlus(GradCAM):
     def __init__(self, net, layer_name):
@@ -127,3 +180,63 @@ class GradCamPlusPlus(GradCAM):
         cam = cv2.resize(cam, (x2 - x1, y2 - y1))
 
         return cam
+
+    def get_mask_all_detection(self, inputs):
+        """
+
+        :param inputs: {"image": [C,H,W], "height": height, "width": width}
+        :param index: 第几个边框
+        :return:
+        """
+        self.net.zero_grad()
+        output = self.net.inference([inputs])
+        print(output)
+
+        # Number of detection
+        num_det = len(output[0]['instances'])
+        list_dict = []
+
+        for index in range(num_det):
+            # Reset the gradient
+            self.net.zero_grad()
+
+            score = output[0]['instances'].scores[index]
+            proposal_idx = output[0]['instances'].indices[index]  # box来自第几个proposal
+            score.backward(retain_graph=True)
+
+            gradient = self.gradient[proposal_idx].cpu().data.numpy()  # [C,H,W]
+            gradient = np.maximum(gradient, 0.)  # ReLU
+            indicate = np.where(gradient > 0, 1., 0.)  # 示性函数
+            norm_factor = np.sum(gradient, axis=(1, 2))  # [C]归一化
+            for i in range(len(norm_factor)):
+                norm_factor[i] = 1. / norm_factor[i] if norm_factor[i] > 0. else 0.  # 避免除零
+            alpha = indicate * norm_factor[:, np.newaxis, np.newaxis]  # [C,H,W]
+
+            weight = np.sum(gradient * alpha, axis=(1, 2))  # [C]  alpha*ReLU(gradient)
+
+            feature = self.feature[proposal_idx].cpu().data.numpy()  # [C,H,W]
+
+            cam = feature * weight[:, np.newaxis, np.newaxis]  # [C,H,W]
+            cam = np.sum(cam, axis=0)  # [H,W]
+            # cam = np.maximum(cam, 0)  # ReLU
+
+            # 数值归一化
+            cam -= np.min(cam)
+            cam /= np.max(cam)
+            # resize to box scale
+            box = output[0]['instances'].pred_boxes.tensor[index].detach().numpy().astype(np.int32)
+            x1, y1, x2, y2 = box
+            cam = cv2.resize(cam, (x2 - x1, y2 - y1))
+
+            class_id = output[0]['instances'].pred_classes[index].detach().numpy()
+            
+            # Putting the result in a dict
+            obj_dict = {}
+            obj_dict['cam'] = cam
+            obj_dict['box'] = box
+            obj_dict['class_id'] = class_id
+            obj_dict['conf'] = score.item()
+            
+            list_dict.append(obj_dict)
+ 
+        return list_dict
